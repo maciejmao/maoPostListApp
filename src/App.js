8f3ca6c02472e516f4ee564eb2f4294a4182dcce
maciejmao <maciejmao@gmail.com>
2019-10-22 15:07:38 +0200

    initial setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section id="searchForm" className="section">
+        <div className="container is-fluid">
+          <h1 className="subtitle">APP</h1>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
